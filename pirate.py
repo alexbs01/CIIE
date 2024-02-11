9990c12bf2e97efa5c071fd7b19b477f0ebdaa63
ruben.fernandez.farelo <ruben.fernandez.farelo@udc.es>
2024-02-11 14:48:23 +0100

    No me carga la secuencia de movimientos

--- pirate.py
+++ pirate.py
@@ -11,8 +11,15 @@ class Pirate(pygame.sprite.Sprite):
         self.speed = speed
         self.direction = 1
         self.flip = False
-        img = pygame.image.load('Imagenes/player/idle_right/1.png').convert_alpha()
-        self.image = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
+        self.animation_list = []
+        self.frame_index = 0
+        self.update_time = pygame.time.get_ticks()
+        # No me carga las imagenes de la carpeta
+        for i in range(1, 5):
+            img = pygame.image.load(f'Imagenes/player/idle_right/{i}.png')
+            img = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
+            self.animation_list.append(img)
+        self.image = self.animation_list[self.frame_index]
         self.rect = self.image.get_rect()
         self.rect.center = (x, y)
 
@@ -39,3 +46,16 @@ class Pirate(pygame.sprite.Sprite):
         # Actualizar la posición del jugador
         self.rect.x += dx
         self.rect.y += dy
+
+    # Actualizar la animación
+    def update_animation(self):
+        ANIMATION_COOLDOWN = 100
+        # Actualizar imagen de la animación dependiendo del frame
+        self.image = self.animation_list[self.frame_index]
+        # Actualizar la animación
+        if pygame.time.get_ticks() - self.update_time > ANIMATION_COOLDOWN:
+            self.update_time = pygame.time.get_ticks()
+            self.frame_index += 1
+            # Si la animación ha terminado, reiniciar
+            if self.frame_index >= len(self.animation_list):
+                self.frame_index = 0
