d71189ad2856bce7669ec16c71c9da75a044e83e
ruben.fernandez.farelo <ruben.fernandez.farelo@udc.es>
2024-02-25 20:22:26 +0100

    Fuente de menu guapa

--- menu.py
+++ menu.py
@@ -15,7 +15,7 @@ game_started = False
 
 
 def get_font(size):  # Devuelve la fuente Press-Start-2P en el tamaño deseado
-    return pygame.font.Font("assets/font.ttf", size)
+    return pygame.font.Font("assets/inmortal.ttf", size)
 
 
 def main_menu():
@@ -28,17 +28,17 @@ def main_menu():
         MENU_TITLE_TEXT = get_font(50).render("MAIN MENU", True, "#b68f40")
         MENU_TITLE_RECT = MENU_TITLE_TEXT.get_rect(center=(400, 50))
 
-        PLAY_BUTTON = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(400, 250),
+        PLAY_BUTTON = Button(image=pygame.image.load("assets/Play Rect.png"), position=(400, 250),
                              text_input="PLAY", font=get_font(50), base_color="#d7fcd4", hovering_color="White")
-        OPTIONS_BUTTON = Button(image=pygame.image.load("assets/Options Rect.png"), pos=(400, 400),
+        OPTIONS_BUTTON = Button(image=pygame.image.load("assets/Options Rect.png"), position=(400, 400),
                                 text_input="OPTIONS", font=get_font(50), base_color="#d7fcd4", hovering_color="White")
-        QUIT_BUTTON = Button(image=pygame.image.load("assets/Quit Rect.png"), pos=(400, 550),
+        QUIT_BUTTON = Button(image=pygame.image.load("assets/Quit Rect.png"), position=(400, 550),
                              text_input="QUIT", font=get_font(50), base_color="#d7fcd4", hovering_color="White")
 
         SCREEN.blit(MENU_TITLE_TEXT, MENU_TITLE_RECT)
 
         for button in [PLAY_BUTTON, OPTIONS_BUTTON, QUIT_BUTTON]:
-            button.changeColor(MOUSE_POSITION)
+            button.change_color(MOUSE_POSITION)
             button.update(SCREEN)
 
         for event in pygame.event.get():
@@ -46,7 +46,7 @@ def main_menu():
                 pygame.quit()
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if PLAY_BUTTON.checkForInput(MOUSE_POSITION):
+                if PLAY_BUTTON.check_for_input(MOUSE_POSITION):
                     game_started = True  # Indica que se debe iniciar el juego
                     break  # Sal del bucle del menú
                 # El resto del código para manejar otros botones...
