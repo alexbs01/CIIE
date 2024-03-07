4d7acd8f94a75ac8ed0f2a67edcb6f14131ec09e
alexbs01 <alex.becerra.suarez01@gmail.com>
2024-03-07 19:16:06 +0100

    Spikes movidos para abajo

--- src/entities/enemies/enemies.py
+++ src/entities/enemies/enemies.py
@@ -85,58 +85,6 @@ class CucumberEnemy(Entity):
         for observer in self.observers:
             observer.update_health(self.health)
 
-class Spike(pygame.sprite.Sprite):
-    def __init__(self, x, y, resource_manager):
-        pygame.sprite.Sprite.__init__(self)
-        self.resource_manager = resource_manager
-        self.animation_list = []
-        self.frame_index = 0
-        self.update_time = pygame.time.get_ticks()
-
-        # Cargar imágenes de los spikes
-        for i in range(4):  # Iterar desde  0 hasta  3
-            img_path = f'assets/enemies/spikes/{i}.png'
-            img = pygame.image.load(img_path)  # Cargar la imagen directamente
-            if img is not None:
-                self.animation_list.append(img)
-            else:
-                print(f"No se pudo cargar la imagen: {img_path}")
-
-        # Asegurarse de que la imagen se ha cargado correctamente
-        if self.animation_list:
-            self.image = self.animation_list[self.frame_index]
-            self.rect = self.image.get_rect()
-        else:
-            self.image = None
-            self.rect = pygame.Rect(x, y, 0, 0)
-
-        self.rect.x = x
-        self.rect.y = y
-        self.collision_rect = pygame.Rect(self.rect.x, self.rect.y, self.rect.width, self.rect.height)
-        self.observers = []
-
-    def draw(self, screen):
-        self.collision_rect = pygame.Rect(self.rect.centerx - self.rect.width / 2,
-                                            self.rect.centery - self.rect.height / 2, self.rect.width,
-                                            self.rect.height)
-        pygame.draw.rect(screen, (255, 0, 0), self.collision_rect, 2)
-        screen.blit(self.image, self.rect)
-
-    def update(self, screen_scroll):
-        self.rect.x += screen_scroll
-        self.collision_rect.x += screen_scroll
-        self.update_animation()
-
-    def update_animation(self):
-        ANIMATION_SPEED = 100  # Velocidad de cambio de imagen en milisegundos
-        # Actualizar la animación
-        if pygame.time.get_ticks() - self.update_time > ANIMATION_SPEED:
-            self.update_time = pygame.time.get_ticks()
-            self.frame_index += 1
-            if self.frame_index >= len(self.animation_list):
-                self.frame_index = 0
-            self.image = self.animation_list[self.frame_index]
-
 class WhaleEnemy(Entity):
     def __init__(self, x, y, speed, resource_manager):
         super().__init__(x, y, speed, resource_manager, 'Whale', first_image_number=1)
@@ -330,3 +278,55 @@ class Capitan(Entity):
     def notify_observers(self):
         for observer in self.observers:
             observer.update_health(self.health)
+
+class Spike(pygame.sprite.Sprite):
+    def __init__(self, x, y, resource_manager):
+        pygame.sprite.Sprite.__init__(self)
+        self.resource_manager = resource_manager
+        self.animation_list = []
+        self.frame_index = 0
+        self.update_time = pygame.time.get_ticks()
+
+        # Cargar imágenes de los spikes
+        for i in range(4):  # Iterar desde  0 hasta  3
+            img_path = f'assets/enemies/spikes/{i}.png'
+            img = pygame.image.load(img_path)  # Cargar la imagen directamente
+            if img is not None:
+                self.animation_list.append(img)
+            else:
+                print(f"No se pudo cargar la imagen: {img_path}")
+
+        # Asegurarse de que la imagen se ha cargado correctamente
+        if self.animation_list:
+            self.image = self.animation_list[self.frame_index]
+            self.rect = self.image.get_rect()
+        else:
+            self.image = None
+            self.rect = pygame.Rect(x, y, 0, 0)
+
+        self.rect.x = x
+        self.rect.y = y
+        self.collision_rect = pygame.Rect(self.rect.x, self.rect.y, self.rect.width, self.rect.height)
+        self.observers = []
+
+    def draw(self, screen):
+        self.collision_rect = pygame.Rect(self.rect.centerx - self.rect.width / 2,
+                                            self.rect.centery - self.rect.height / 2, self.rect.width,
+                                            self.rect.height)
+        pygame.draw.rect(screen, (255, 0, 0), self.collision_rect, 2)
+        screen.blit(self.image, self.rect)
+
+    def update(self, screen_scroll):
+        self.rect.x += screen_scroll
+        self.collision_rect.x += screen_scroll
+        self.update_animation()
+
+    def update_animation(self):
+        ANIMATION_SPEED = 100  # Velocidad de cambio de imagen en milisegundos
+        # Actualizar la animación
+        if pygame.time.get_ticks() - self.update_time > ANIMATION_SPEED:
+            self.update_time = pygame.time.get_ticks()
+            self.frame_index += 1
+            if self.frame_index >= len(self.animation_list):
+                self.frame_index = 0
+            self.image = self.animation_list[self.frame_index]
